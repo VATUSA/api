107af116c9a69d6c3060cb5d97658fe55ff34083
Daniel Hawton <daniel@hawton.com>
2017-08-13 17:26:15 -0800

    Semantic build, header setup

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <title>VATUSA - @yield('title')</title>
+    <link href='/semantic/semantic.min.css' rel='stylesheet' type='text/css'>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <!-- Header -->
+    <section class="blueHeader">
+      <div class="ui container">
+        <div class="ui stackable grid">
+          <div class="blue twelve wide column">
+            <img src="/images/logo-dark.png" class="ui image" style="margin-right: 1.5em">
+          </div>
+          <div class="blue four wide column middle aligned">
+            <div class="ui card ">
+              <div class="content">
+                <div class="header">Daniel Hawton (876594)</div>
+                <div class="meta">Controller (C1) - VATUSA Headquarters</div>
+              </div>
+            </div>
+            </div>
+        </div>
+      </div>
+      <div class="ui large red inverted stackable menu" style="margin-bottom: 1.5em">
+        <div class="ui container">
+          <a class="item" href="/">Home</a>
+          <a class="item" href="/">Test</a>
+        </div>
+      </div>
+    </section>
+    <!-- Footer -->
+    <script src="/js/app.js" type="text/javascript"></script>
+    <script src="/semantic/semantic.min.js" type="text/javascript"></script>
+  </body>
+</html>
