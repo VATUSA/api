9e9defaf9d895a1699a21a87e175e174ed59a731
Daniel Hawton <daniel@hawton.com>
2017-08-16 18:55:26 -0800

    Tweak layout and semantic for header

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,20 +12,21 @@
     <section class="blueHeader">
       <div class="ui container">
         <div class="ui stackable grid">
-          <div class="blue twelve wide column">
+          <div class="twelve wide column">
             <img src="/images/logo-dark.png" class="ui image" style="margin-right: 1.5em">
           </div>
-          <div class="blue four wide column middle aligned">
-            <div class="ui card ">
+          <div class="four wide column middle aligned">
+            <div class="ui red card">
               <div class="content">
-                <div class="header">Daniel Hawton (876594)</div>
-                <div class="meta">Controller (C1) - VATUSA Headquarters</div>
+                <div><h3 class="ui red header">Daniel Hawton (876594)</h3></div>
+                <div>Controller (C1)</div>
+                <div class="ui blue text">HCF - Honolulu Control Facility</div>
               </div>
             </div>
             </div>
         </div>
       </div>
-      <div class="ui large red inverted stackable menu" style="margin-bottom: 1.5em">
+      <div class="ui large inverted stackable menu" style="margin-bottom: 1.5em">
         <div class="ui container">
           <a class="item" href="/">Home</a>
           <a class="item" href="/">Test</a>
