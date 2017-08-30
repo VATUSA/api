e733001644da8ad701eccff2e5286ab891e9b67f
Daniel Hawton <daniel@hawton.com>
2017-08-29 22:51:42 -0800

    Assume push event

--- app/Http/Controllers/API/DeployController.php
+++ app/Http/Controllers/API/DeployController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
@@ -6,16 +7,15 @@ use Illuminate\Http\Request;
 
 class DeployController extends Controller
 {
-    public function getDeploy(Request $request) {
-        if ($request->header("HTTP_X_GITHUB_EVENT") == "push") {
-            $payload = json_parse($request->input("payload"));
-            if (!$payload) {
-                \Log::warn("Invalid payload received from github push event: $payload");
-                return;
-            }
-            exec("cd " . base_path() . " && sh deploy.sh", $output, $return);
-            \Log::info("Deploy ran: " . base64_encode($output) . ", return $return");
-            return implode($output);
+    public function getDeploy(Request $request)
+    {
+        $payload = json_parse($request->input("payload"));
+        if (!$payload) {
+            \Log::warn("Invalid payload received from github push event: $payload");
+            return;
         }
+        exec("cd " . base_path() . " && sh deploy.sh", $output, $return);
+        \Log::info("Deploy ran: " . base64_encode($output) . ", return $return");
+        return implode($output);
     }
 }
