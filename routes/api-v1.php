6d6691b6c892d570afaa3465f495379641a1e89b
Daniel Hawton <daniel@hawton.com>
2017-08-29 21:33:50 -0800

    Fix FactilityHelper, IDs aren't numbers

--- routes/api-v1.php
+++ routes/api-v1.php
@@ -1,34 +1,34 @@
 <?php
 Route::group(['prefix' => '{apikey}/', 'middleware' => 'apikey'], function () {
 // CBT
-Route::get('cbt/block', 'CBTController@getCBTBlocks');
-Route::get('cbt/block/{id}', 'CBTController@getCBTChapters')->where('id', '[0-9]+');
-Route::get('cbt/chapter/{id}', 'CBTController@getCBTChapter')->where('id', '[0-9]+');
-Route::put('cbt/progress/{cid}', 'CBTController@putCBTProgress')->where('cid', '[0-9]+');
+    Route::get('cbt/block', 'CBTController@getCBTBlocks');
+    Route::get('cbt/block/{id}', 'CBTController@getCBTChapters')->where('id', '[0-9]+');
+    Route::get('cbt/chapter/{id}', 'CBTController@getCBTChapter')->where('id', '[0-9]+');
+    Route::put('cbt/progress/{cid}', 'CBTController@putCBTProgress')->where('cid', '[0-9]+');
 
-Route::get('controller/{cid}', 'FacilityController@getController')->where('cid', '[0-9]+');
+    Route::get('controller/{cid}', 'FacilityController@getController')->where('cid', '[0-9]+');
 
 // Exam
-Route::get('exam/results/{cid}', 'ExamController@getUserResults')->where('cid', '[0-9]+');
-Route::get('exam/result/{rid}', 'ExamController@getExamResults')->where('rid', '[0-9]+');
+    Route::get('exam/results/{cid}', 'ExamController@getUserResults')->where('cid', '[0-9]+');
+    Route::get('exam/result/{rid}', 'ExamController@getExamResults')->where('rid', '[0-9]+');
 
 // Roster
-Route::get('roster', 'FacilityController@getRoster');
-Route::get('roster/{fac}', 'FacilityController@getRoster')->where('fac', '[A-Z]{3}');
-Route::delete('roster/{cid}', 'FacilityController@deleteRoster')->where('cid', '[0-9]+');
-Route::delete('roster/{fac}/{cid}', 'FacilityController@deleteRoster')->where('fac', '[A-Z]{3}')->where('cid', '[0-9]+');
+    Route::get('roster', 'FacilityController@getRoster');
+    Route::get('roster/{fac}', 'FacilityController@getRoster')->where('fac', '[A-Z]{3}');
+    Route::delete('roster/{cid}', 'FacilityController@deleteRoster')->where('cid', '[0-9]+');
+    Route::delete('roster/{fac}/{cid}', 'FacilityController@deleteRoster')->where('fac', '[A-Z]{3}')->where('cid', '[0-9]+');
 
 // Solo Certs
-Route::get('solo/{cid}','SoloController@getSolo')->where('cid', '[0-9]+');
-Route::post('solo/{cid}/{position}','SoloController@postSolo')->where('cid', '[0-9]+')->where("position", "[0-9A-Z_]+");
-Route::delete('solo/{cid}/{position}','SoloController@deleteSolo')->where('cid', '[0-9]+')->where("position", "[0-9A-Z_]+");
+    Route::get('solo/{cid}', 'SoloController@getSolo')->where('cid', '[0-9]+');
+    Route::post('solo/{cid}/{position}', 'SoloController@postSolo')->where('cid', '[0-9]+')->where("position", "[0-9A-Z_]+");
+    Route::delete('solo/{cid}/{position}', 'SoloController@deleteSolo')->where('cid', '[0-9]+')->where("position", "[0-9A-Z_]+");
 
 // Transfer
-Route::get('transfer', 'TransferController@getTransfers');
-Route::get('transfer/{fac}', 'TransferController@getTransfers')->where('fac', '[A-Z]{3}');
-Route::post('transfer/{id}', 'TransferController@postTransfer')->where('id', '[0-9]+');
+    Route::get('transfer', 'TransferController@getTransfers');
+    Route::get('transfer/{fac}', 'TransferController@getTransfers')->where('fac', '[A-Z]{3}');
+    Route::post('transfer/{id}', 'TransferController@postTransfer')->where('id', '[0-9]+');
 
-Route::get('conntest', 'PublicController@getConnectionTest');
+    Route::get('conntest', 'PublicController@getConnectionTest');
 });
 Route::get('news.{ext},{limit}', 'PublicController@getNews')->where(['ext' => '[A-Za-z]+', 'limit' => '\d+']);
 Route::get('news,{limit}', 'PublicController@getNews')->where(['ext' => '[A-Za-z]+', 'limit' => '\d+']);
