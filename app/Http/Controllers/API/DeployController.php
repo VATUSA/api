1a195f04cc1cbea9651d9119f96d994dafe40aad
Daniel Hawton <daniel@hawton.com>
2017-08-29 23:02:23 -0800

    don't need to check payload... only pulling master

--- app/Http/Controllers/API/DeployController.php
+++ app/Http/Controllers/API/DeployController.php
@@ -9,13 +9,7 @@ class DeployController extends Controller
 {
     public function getDeploy(Request $request)
     {
-        $payload = decode_json($request->input("payload"));
-        if (!$payload) {
-            \Log::warn("Invalid payload received from github push event: $payload");
-            return;
-        }
         exec("cd " . base_path() . " && sh deploy.sh", $output, $return);
-        \Log::info("Deploy ran: " . base64_encode($output) . ", return $return");
         return implode($output);
     }
 }
