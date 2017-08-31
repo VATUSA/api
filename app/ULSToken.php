574a8bf75e1d238c75a0f955819d2982f81c67e3
Daniel Hawton <daniel@hawton.com>
2017-08-30 18:22:26 -0800

    set primarykey in ulstokens model

--- app/ULSToken.php
+++ app/ULSToken.php
@@ -8,6 +8,7 @@ class ULSToken extends Model
 {
     protected $table = "uls_tokens";
     public $incrementing = false;
+    protected $primaryKey = 'token';
     public $timestamps = false;
     protected $dates = [
         'date'
