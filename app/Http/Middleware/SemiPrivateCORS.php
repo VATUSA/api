b5b672152db8516dbd2127cffacfee78825295df
Daniel Hawton <daniel@hawton.com>
2017-09-04 02:06:44 -0800

    fix middleware

--- app/Http/Middleware/SemiPrivateCORS.php
+++ app/Http/Middleware/SemiPrivateCORS.php
@@ -22,7 +22,7 @@ class SemiPrivateCORS
         header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
         header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 
-        if ($request->method("OPTIONS")) {
+        if ($request->isMethod("OPTIONS")) {
             return $next($request);
         }
 
