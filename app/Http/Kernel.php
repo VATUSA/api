2141a7f856161834e195cc925de01d119a0222d1
Daniel Hawton <daniel@hawton.com>
2017-09-05 05:06:09 -0800

    Disable CSRF

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -33,7 +33,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
+            //\App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
