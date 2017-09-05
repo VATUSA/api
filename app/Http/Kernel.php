36cd48cda2ff59c9285c180588cd445802f79848
Daniel Hawton <daniel@hawton.com>
2017-09-05 01:19:54 -0800

    Revert "reorder middleware maybe?!"
    This reverts commit afeab4d393473c9b5a132433a65de42d18072252.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,9 +20,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Session\Middleware\StartSession::class,
-        \App\Http\Middleware\EncryptCookies::class,
-        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-        \Illuminate\Session\Middleware\AuthenticateSession::class,
     ];
 
     /**
@@ -32,6 +29,10 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             //\App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
