95cdedba1b01526b188d148e772bde89fd5a85b3
Daniel Hawton <daniel@hawton.com>
2017-11-01 17:13:52 -0800

    add exam return to uls

--- app/Http/Controllers/Login/SSOController.php
+++ app/Http/Controllers/Login/SSOController.php
@@ -61,6 +61,8 @@ class SSOController extends Controller
             $request->session()->put('return', env('SSO_RETURN_LOCALDEV'));
         } elseif ($request->has('uls')) {
             $request->session()->put('return', env('SSO_RETURN_ULS'));
+        } elseif ($request->has('exam')) {
+            $request->session()->put('exam', env('SSO_RETURN_EXAM', 'https://www.vatusa.net/exam/0'));
         } else {
             $request->session()->put('return', env('SSO_RETURN_FORUMS'));
         }
