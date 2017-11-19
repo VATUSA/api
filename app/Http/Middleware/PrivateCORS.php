d32c9860d6328e8b9ab9f715234df57f9b0d9ed5
Daniel Hawton <daniel@hawton.com>
2017-11-18 15:05:15 -0900

    fix regexp

--- app/Http/Middleware/PrivateCORS.php
+++ app/Http/Middleware/PrivateCORS.php
@@ -13,9 +13,8 @@ class PrivateCORS
     public function handle($request, Closure $next)
     {
         if(in_array($_SERVER['REQUEST_METHOD'], ["GET","PUT","DELETE","POST"])) {
-            if (!isset($_SERVER['HTTP_ORIGIN']) || preg_match("/^http(s?):\/\/[^/]+\.vatusa\.net/", $_SERVER['HTTP_ORIGIN'])) {
-
-                abort(400, "Malformed origin");
+            if (!isset($_SERVER['HTTP_ORIGIN']) || !preg_match("~^(http|https)://[^/]+\.vatusa\.net~i", $_SERVER['HTTP_ORIGIN'])) {
+                abort(400, "Malformed origin " . $_SERVER['HTTP_ORIGIN']);
             }
         }
         return $next($request);
