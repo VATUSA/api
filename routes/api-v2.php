1b08a550ae7598f92f792ef16087df08433c0c06
Daniel Hawton <daniel@hawton.com>
2017-12-25 19:38:23 -0900

    CBT  Functions

--- routes/api-v2.php
+++ routes/api-v2.php
@@ -75,9 +75,15 @@ Route::group(['prefix' => '/stats'], function() {
 Route::group(['prefix' => '/users'], function() {
     Route::get('/{cid}', 'UserController@getIndex')->where('cid', '[0-9]+');
     Route::get('/roles/{facility}/{role}', 'UserController@getRoleUsers')->where(['facility' => '[A-Za-z]{3}', 'role' => '[A-Za-z0-9]+']);
-    Route::get('/{cid}/transfer/checklist', 'UserController@getTransferChecklist')->where('cid','[0-9]+');
-    Route::get('/{cid}/rating/history', 'UserController@getRatingHistory')->where('cid','[0-9]+');
 
+    Route::get('/{cid}/cbt/history', 'UserController@getCBTHistory')->where('cid','[0-9]+');
+    Route::get('/{cid}/cbt/progress/{blockId}', 'UserController@getCBTProgress')->where(['cid' => '[0-9]+', 'blockId' => '[0-9]+']);
+    Route::put('/{cid}/cbt/progress/{blockId}/{chapterId}', 'UserController@getCBTProgress')->where(['cid' => '[0-9]+', 'blockId' => '[0-9]+', 'chapterId' => '[0-9]+']);
+
+    Route::get('/{cid}/transfer/checklist', 'UserController@getTransferChecklist')->where('cid','[0-9]+');
+    Route::get('/{cid}/transfer/history', 'UserController@getTransferHistory')->where('cid', '[0-9]+');
+
+    Route::get('/{cid}/rating/history', 'UserController@getRatingHistory')->where('cid','[0-9]+');
 
     Route::group(['middleware' => 'private'], function() {
         Route::get('/{cid}/log', 'UserController@getActionLog')->where('cid', '[0-9]+');
