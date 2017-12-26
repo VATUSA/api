c92b38720210b3727d91fedb797951ba7aaaae4f
Daniel Hawton <daniel@hawton.com>
2017-12-26 01:54:00 -0900

    Users done

--- app/Role.php
+++ app/Role.php
@@ -1,11 +1,16 @@
 <?php
 namespace App;
 
-class Role extends Model {
+class Role extends Model
+{
     protected $table = 'roles';
     public $timestamps = ['created_at'];
 
     public function user() {
         return $this->belongsTo('App\User', 'cid', 'cid');
+    }
+
+    public function setUpdatedAtAttribute($value) {
+        // to Disable updated_at
     }
 }
--- routes/api-v2.php
+++ routes/api-v2.php
@@ -74,18 +74,26 @@ Route::group(['prefix' => '/stats'], function() {
  */
 Route::group(['prefix' => '/users'], function() {
     Route::get('/{cid}', 'UserController@getIndex')->where('cid', '[0-9]+');
+
     Route::get('/roles/{facility}/{role}', 'UserController@getRoleUsers')->where(['facility' => '[A-Za-z]{3}', 'role' => '[A-Za-z0-9]+']);
+    Route::post('/{cid}/roles/{facility}/{role}', 'UserController@postRole')->where(['facility' => '[A-Za-z]{3}', 'facility' => '[A-Z0-9]{3}', 'role' => '[A-Za-z0-9]+']);
+    Route::delete('/{cid}/roles/{facility}/{role}', 'UserController@deleteRole')->where(['facility' => '[A-Za-z]{3}', 'facility' => '[A-Z0-9]{3}', 'role' => '[A-Za-z0-9]+']);
 
     Route::get('/{cid}/cbt/history', 'UserController@getCBTHistory')->where('cid','[0-9]+');
     Route::get('/{cid}/cbt/progress/{blockId}', 'UserController@getCBTProgress')->where(['cid' => '[0-9]+', 'blockId' => '[0-9]+']);
     Route::put('/{cid}/cbt/progress/{blockId}/{chapterId}', 'UserController@getCBTProgress')->where(['cid' => '[0-9]+', 'blockId' => '[0-9]+', 'chapterId' => '[0-9]+']);
 
+    Route::get('/{cid}/exam/history', 'UserController@getExamHistory')->where('cid', '[0-9]+');
+
     Route::get('/{cid}/transfer/checklist', 'UserController@getTransferChecklist')->where('cid','[0-9]+');
     Route::get('/{cid}/transfer/history', 'UserController@getTransferHistory')->where('cid', '[0-9]+');
 
+    Route::post('/{cid}/rating', 'UserController@postRating')->where('cid', '[0-9]+');
     Route::get('/{cid}/rating/history', 'UserController@getRatingHistory')->where('cid','[0-9]+');
 
     Route::group(['middleware' => 'private'], function() {
+        Route::post('/{cid}/transfer', 'UserController@postTransfer')->where('cid', '[0-9]+');
+
         Route::get('/{cid}/log', 'UserController@getActionLog')->where('cid', '[0-9]+');
         Route::post('/{cid}/log','UserController@postActionLog')->where('cid','[0-9]+');
     });
