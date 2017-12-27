5af1ef45bdc94afcf2d991c199eb745f7a3dd32e
Daniel Hawton <daniel@hawton.com>
2017-12-26 17:29:30 -0900

    Facility Controller additions

--- app/Facility.php
+++ app/Facility.php
@@ -2,11 +2,24 @@
 
 namespace App;
 
+/**
+ * Class Facility
+ * @package App
+ *
+ * @SWG\Definition(
+ *     type="object",
+ *     @SWG\Property(property="id", type="string", description="Facility IATA ID"),
+ *     @SWG\Property(property="name", type="string"),
+ *     @SWG\Property(property="url", type="string"),
+ *     @SWG\Property(property="region", type="integer", description="Region represented by ATD (ie 7 = VATUSA7)"),
+ * )
+ */
 class Facility extends Model
 {
     protected $table = 'facilities';
     public $timestamps = false;
     public $incrementing = false;   // id is IATA of facility
+    protected $visible = ["id", "name", "url", "region"];
 
     public function members()
     {
