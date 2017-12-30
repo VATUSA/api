7c4c89bb998d1e56469c368f59935f7cbce901c5
Daniel Hawton <daniel@hawton.com>
2017-12-30 13:44:49 -0900

    migration, seeds, prep

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->command->info("Seeding tables...");
+        $this->seed("controllers");
+        $this->seed("exams");
+        $this->seed("facilities");
+        $this->seed("knowledgebase", "kb_cats");
+        $this->seed("knowledgebase FAQ", "kb_qs");
+        $this->seed("promotions");
+        $this->seed("roles");
+        $this->seed("role_titles");
+        $this->seed("training");
+        $this->seed("transfers");
+        $this->command->info("Done.");
+    }
+
+    public function seed($info, $table = null) {
+        if ($table == null) $table = $info;
+        $this->command->info("Seeding $info...");
+        \DB::unprepared(file_get_contents("database/seeds/$table.sql"));
     }
 }
