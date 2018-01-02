5991350e81ec39d78b373ec03e78c1126d699d9c
Daniel Hawton <daniel@hawton.com>
2018-01-01 23:33:53 -0900

    fix docker log

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,14 +41,6 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
-// For Docker
-$app->configureMonologUsing(function($monolog) {
-    // Always add the stderr output for errors over WARNING level.
-    $monolog->pushHandler(
-        new \Monolog\Handler\StreamHandler('php://stderr', \Monolog\Logger::WARNING)
-    );
-});
-
 /*
 |--------------------------------------------------------------------------
 | Return The Application
