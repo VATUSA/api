80976f368919d384839d5f352c31e1cfbd3ea507
Daniel Hawton <daniel@hawton.com>
2018-01-02 20:23:42 -0900

    Tweak exam methods for variables,

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,6 +4,10 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    private $user;
+    private $pass;
+    private $db;
+    private $host;
     /**
      * Run the database seeds.
      *
@@ -11,6 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->user = config('database.connections.mysql.username');
+        $this->pass = config('database.connections.mysql.password');
+        $this->db = config('database.connections.mysql.database');
+        $this->host = config('database.connections.mysql.host');
         $this->command->info("Seeding tables...");
         $this->seed("controllers");
         $this->seed("exams");
@@ -18,6 +26,7 @@ class DatabaseSeeder extends Seeder
         $this->seed("knowledgebase", "kb_cats");
         $this->seed("knowledgebase FAQ", "kb_qs");
         $this->seed("promotions");
+        $this->seed("ratings");
         $this->seed("roles");
         $this->seed("role_titles");
         $this->seed("training");
@@ -28,6 +37,8 @@ class DatabaseSeeder extends Seeder
     public function seed($info, $table = null) {
         if ($table == null) $table = $info;
         $this->command->info("Seeding $info...");
-        \DB::unprepared(file_get_contents("database/seeds/$table.sql"));
+        //\DB::unprepared(file_get_contents("database/seeds/$table.sql"));
+        exec("mysql -u $this->user -p$this->pass -h $this->host $this->db < database/seeds/$table.sql");
+
     }
 }
