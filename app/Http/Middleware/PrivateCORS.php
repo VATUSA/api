35b6eebd5d454cd48f28a7a08eb2fc30d36ed424
Daniel Hawton <daniel@hawton.com>
2018-02-14 08:41:08 -0900

    Add devel TLD to permitted origins on private middleware

--- app/Http/Middleware/PrivateCORS.php
+++ app/Http/Middleware/PrivateCORS.php
@@ -1,24 +1,53 @@
 <?php
+
 namespace App\Http\Middleware;
+
 use Closure;
+
 class PrivateCORS
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        if (env('APP_ENV', 'prod') === "dev") {
-            return $next($request);
-        }
+        if (env('APP_ENV', 'prod') === "prod") {
+            if (in_array(
+                $_SERVER['REQUEST_METHOD'], ["GET", "PUT", "DELETE", "POST"]
+            )
+            ) {
+                if (!isset($_SERVER['HTTP_ORIGIN'])
+                    || !preg_match(
+                        "~^(http|https)://[^/]+\.vatusa\.net~i",
+                        $_SERVER['HTTP_ORIGIN']
+                    )
+                ) {
+                    abort(400, "Malformed origin " . $_SERVER['HTTP_ORIGIN']);
+                }
+            }
+        } else {
+            if (in_array(
+                $_SERVER['REQUEST_METHOD'], ["GET", "PUT", "DELETE", "POST"]
+            )
+            ) {
+                if (!isset($_SERVER['HTTP_ORIGIN'])
+                    || !preg_match(
+                        "~^(http|https)://[^/]+\.vatusa\.net~i",
+                        $_SERVER['HTTP_ORIGIN']
+                    )
+                    || !preg_match(
+                        "~^(http|https)://[^/]+\.vatusa\.devel~i",
+                        $_SERVER['HTTP_ORIGIN']
 
-        if(in_array($_SERVER['REQUEST_METHOD'], ["GET","PUT","DELETE","POST"])) {
-            if (!isset($_SERVER['HTTP_ORIGIN']) || !preg_match("~^(http|https)://[^/]+\.vatusa\.net~i", $_SERVER['HTTP_ORIGIN'])) {
-                abort(400, "Malformed origin " . $_SERVER['HTTP_ORIGIN']);
+                    )
+                ) {
+                    abort(400, "Malformed origin " . $_SERVER['HTTP_ORIGIN']);
+                }
             }
         }
 
