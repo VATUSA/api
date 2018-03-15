612fc8acd16f35865e72a77c6db35af57f95594f
Daniel Hawton <daniel@hawton.com>
2018-03-15 15:19:10 -0800

    suryvey continued

---
+++ database/migrations/2018_02_28_173734_create_survey_questions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSurveyQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('survey_questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer("survey_id");
+            $table->string("question");
+            $table->string("data");
+            $table->integer("order");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('survey_questions');
+    }
+}
---
+++ database/migrations/2018_02_28_173825_create_survey_assignments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSurveyAssignmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('survey_assignments', function (Blueprint $table) {
+            $table->string('id');
+            $table->string("facility");
+            $table->integer("rating");
+            $table->string("misc_data");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('survey_assignments');
+    }
+}
