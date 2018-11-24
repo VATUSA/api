ffe915de41c411f5b1c8c5393553539a39a6bc4a
Blake Nahin <bnahin@live.com>
2018-11-23 22:44:40 -0800

    Update SoloCert fillable properties

--- app/SoloCert.php
+++ app/SoloCert.php
@@ -4,6 +4,8 @@ namespace App;
 class SoloCert extends Model {
     protected $table = 'solo_certs';
 
+    protected $fillable = ['cid', 'position', 'expires'];
+
     public function user() {
         return $this->belongsTo('App\User', 'cid', 'cid');
     }
