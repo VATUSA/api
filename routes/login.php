7dc91610c70d8df73d4f0e1ab4b7fa62ce85f5b8
Blake Nahin <bnahin@live.com>
2019-01-19 23:16:34 -0800

    Remove ULSv1

--- routes/login.php
+++ routes/login.php
@@ -13,10 +13,6 @@ Route::middleware(['login'])->group(function() {
 });
 
 Route::prefix("/uls")->middleware(['login'])->group(function () {
-    Route::get("login", "ULSController@getLogin");
-    Route::get("redirect", "ULSController@getRedirect");
-    Route::get("info", "ULSController@getInfo");
-
     Route::prefix("/v2")->group(function() {
        Route::get('login', 'ULSv2Controller@getLogin');
        Route::get('redirect', 'ULSv2Controller@getRedirect');
