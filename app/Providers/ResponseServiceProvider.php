b0b8925b31624a25f33065aa3c1d94954c23fdf6
Blake Nahin <vatusa12@vatusa.net>
2019-03-31 23:30:35 -0700

    Sandbox check and response

--- app/Providers/ResponseServiceProvider.php
+++ app/Providers/ResponseServiceProvider.php
@@ -17,24 +17,25 @@ use Jose\Component\Signature\Serializer\JSONFlattenedSerializer;
 
 class ResponseServiceProvider extends ServiceProvider
 {
-    public function boot(ResponseFactory $factory) {
-        $factory->macro('malformed', function($data = []) use ($factory) {
+    public function boot(ResponseFactory $factory)
+    {
+        $factory->macro('malformed', function ($data = []) use ($factory) {
             return response()->api(array_merge(generate_error("Malformed request"), $data), 400);
         });
 
-        $factory->macro('conflict', function($data = []) use ($factory) {
+        $factory->macro('conflict', function ($data = []) use ($factory) {
             return response()->api(array_merge(generate_error("Conflict"), $data), 409);
         });
 
-        $factory->macro('notfound', function($data = []) use ($factory) {
+        $factory->macro('notfound', function ($data = []) use ($factory) {
             return response()->api(array_merge(generate_error("Not found"), $data), 404);
         });
 
-        $factory->macro('forbidden', function($data = []) use ($factory) {
+        $factory->macro('forbidden', function ($data = []) use ($factory) {
             return response()->api(array_merge(generate_error("Forbidden"), $data), 403);
         });
 
-        $factory->macro('unauthenticated', function($data = []) use ($factory) {
+        $factory->macro('unauthenticated', function ($data = []) use ($factory) {
             return response()->api(array_merge(generate_error("Unauthorized"), $data), 401);
         });
 
@@ -43,26 +44,31 @@ class ResponseServiceProvider extends ServiceProvider
         });
 
         $factory->macro('api', function ($data, $status = 200, $headers = []) use ($factory) {
-            $showsig = false; $fjwk = null;
+            $showsig = false;
+            $fjwk = null;
             if (Request::filled("f")) {
                 $facility = Facility::find(Request::input("f"));
                 if ($facility) {
                     $fjwk = $facility->apiv2_jwk;
                     $showsig = true;
                 }
-            } else if (isset($_SERVER['HTTP_ORIGIN'])) {
-                $domain = extract_domain(parse_url($_SERVER['HTTP_ORIGIN'], PHP_URL_HOST));
-                $facility = Facility::where('url', 'LIKE', "%$domain%")->first();
-                if ($facility) {
-                    $showsig = true;
-                    $fjwk = $facility->apiv2_jwk;
+            } else {
+                if (isset($_SERVER['HTTP_ORIGIN'])) {
+                    $domain = extract_domain(parse_url($_SERVER['HTTP_ORIGIN'], PHP_URL_HOST));
+                    $facility = Facility::where('url', 'LIKE', "%$domain%")->first();
+                    if ($facility) {
+                        $showsig = true;
+                        $fjwk = $facility->apiv2_jwk;
+                    }
                 }
             }
 
             $sig = [];
             if ($showsig && $fjwk != null) {
                 $algorithmManager = AlgorithmManager::create([
-                    new HS256(), new HS384(), new HS512(),
+                    new HS256(),
+                    new HS384(),
+                    new HS512(),
                 ]);
 
                 $jwk = JWK::create(json_decode($facility->apiv2_jwk, true));
@@ -74,17 +80,22 @@ class ResponseServiceProvider extends ServiceProvider
                     $algorithmManager
                 );
 
-                $payload = $jsonConverter->encode($data);
-                $jws = $jwsBuilder->create()->withPayload($payload)->addSignature($jwk, ['alg'=>json_decode($facility->apiv2_jwk, true)['alg']])->build();
+                $payload = $jsonConverter->encode(array_merge(['testing' => isTest(), $data]));
+                $jws = $jwsBuilder->create()->withPayload($payload)->addSignature($jwk,
+                    ['alg' => json_decode($facility->apiv2_jwk, true)['alg']])->build();
                 $serializer = new JSONFlattenedSerializer($jsonConverter);
-                return $factory->make($serializer->serialize($jws, 0), $status, array_merge($headers, ['Content-Type' => 'application/json']));
+
+                return $factory->make($serializer->serialize($jws, 0), $status,
+                    array_merge($headers, ['Content-Type' => 'application/json']));
             }
 
-            return $factory->make(encode_json(array_merge($data, $sig)), $status, array_merge($headers, ['Content-Type' => 'application/json']));
+            return $factory->make(encode_json(array_merge($data, $sig, ['testing' => isTest()])), $status,
+                array_merge($headers, ['Content-Type' => 'application/json']));
         });
     }
 
-    public function register() {
+    public function register()
+    {
         //
     }
 }
