426a55873107d278792ec546e7756b806e6c1f23
Blake Nahin <vatusa12@vatusa.net>
2019-07-05 16:25:55 -0700

    Bump version again

--- app/Http/Controllers/API/v2/APIController.php
+++ app/Http/Controllers/API/v2/APIController.php
@@ -13,7 +13,7 @@ use \App\Http\Controllers\Controller as BaseController;
  *     host="api.vatusa.net",
  *     schemes={"https"},
  *     @SWG\Info(
- *         version="2.0.1",
+ *         version="2.1",
  *         title="VATUSA API",
  *         description="VATUSA APIv2 Documentation.
             Authentication methods are: <ul>
