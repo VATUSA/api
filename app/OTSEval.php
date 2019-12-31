cd40c60eb0c6afe785a503f6842dbdb017bf833c
Blake Nahin <vatusa6@vatusa.net>
2019-12-31 12:38:19 -0800

    Change TR modification rules

--- app/OTSEval.php
+++ app/OTSEval.php
@@ -6,6 +6,20 @@ use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\File;
 
+/**
+ * Class OTSEval
+ * @package App
+ *
+ * @SWG\Definition(
+ *     type="object",
+ *     @SWG\Property(property="id", type="integer", description="Record ID"),
+ *     @SWG\Property(property="filename", type="string", description="Filename in system"),
+ *     @SWG\Property(property="training_record_id", type="integer", description="Training record DB ID, if exists"),
+ *     @SWG\Property(property="student_id", type="integer", description="Student CID"),
+ *     @SWG\Property(property="instructor_id", type="integer", description="Instructor CID"),
+ *     @SWG\Property(property="rating_id", type="integer", description="DB ID of rating")
+ * )
+ */
 class OTSEval extends Model
 {
     public function trainingRecord()
@@ -16,12 +30,19 @@ class OTSEval extends Model
         //position to level (APP = S3).
     }
 
-    public function student() {
+    public function student()
+    {
         return $this->belongsTo(User::class, 'student_id', 'cid');
     }
 
-    public function instructor() {
+    public function instructor()
+    {
         return $this->belongsTo(User::class, 'instructor_id', 'cid');
+    }
+
+    public function rating()
+    {
+        return $this->belongsTo(Rating::class);
     }
 
     public function getContent()
