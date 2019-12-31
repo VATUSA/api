4246e134b8c2662872988026fa17368b113caba5
Blake Nahin <vatusa12@vatusa.net>
2019-12-30 23:48:08 -0800

    Begin Centralized Training Records

---
+++ database/migrations/2019_12_31_064756_create_training_records_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrainingRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('training_records', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('student_id');
+            $table->integer('instructor_id');
+
+            $table->datetime('session_date');
+
+            $table->string('facility_id');
+            $table->string('position');
+            $table->time('session_duration')->nullable();
+            $table->integer('num_movements')->nullable();
+            $table->integer('overall_score')->nullable();
+            $table->text('notes');
+
+            $table->smallInteger('session_location'); // 0 = Classroom, 1 = Live, 2 = Sweatbox
+            $table->boolean('isOTS');
+            $table->boolean('isCBT'); //CBT Completion - Auto
+            $table->boolean('soloGranted');
+
+            $table->boolean('otsResult')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('training_records');
+    }
+}
