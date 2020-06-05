5d372a234127846266a7ad5f0b4af973f9cddbd4
Yi Zhang <z@wtmsb.io>
2020-06-05 04:32:50 -0400

    docker-compose.yml for local docker setup, cleanup

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cat /run/secrets/key > /www/.env
-cat /run/secrets/*.env >> /www/.env
-cat /run/secrets/sso.rsa >> /www/.sso.rsa
+cat /run/secrets/key >/www/.env
+cat /run/secrets/*.env >>/www/.env
+cat /run/secrets/sso.rsa >>/www/.sso.rsa
 
 chown application:application /www/.env
 
@@ -13,8 +13,8 @@ chmod 600 /run/secrets/*.key
 chown application:application /run/secrets/*.key
 
 if [[ "$WWW_ENV" == "prod" ]]; then
-  echo "*    *    *     *     *    cd /www && php artisan schedule:run" >> /etc/crontabs/application
-  echo "*    *    *     *     *    cd /www && php artisan vatsim:flights" >> /etc/crontabs/application
+  echo "*    *    *     *     *    cd /www && php artisan schedule:run" >>/etc/crontabs/application
+  echo "*    *    *     *     *    cd /www && php artisan vatsim:flights" >>/etc/crontabs/application
   cd /www && php artisan migrate --force
   chown -R application:application /www/storage/logs
   chmod -R 777 /www/storage/app/purifier/ # HTML Purifier
