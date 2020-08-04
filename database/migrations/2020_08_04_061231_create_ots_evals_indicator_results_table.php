9cc9b4716669ed5138d83c16c66ffcaa8f6645d8
Blake Nahin <bnahin@live.com>
2020-08-04 01:59:40 -0500

    OTS Eval Migrations

---
+++ database/migrations/2020_08_04_061231_create_ots_evals_indicator_results_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOtsEvalsIndicatorResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ots_evals_indicator_results', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('perf_indicator_id');
+            $table->unsignedInteger('eval_id');
+            $table->smallInteger('result'); // 0 = Not Observed, 1 = Commendable, 2 = Satisfactory, 3 = Unsatisfactory
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ots_evals_indicator_results');
+    }
+}
---
+++ database/migrations/2020_08_04_062511_create_ots_evals_foreign_keys.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOtsEvalsForeignKeys extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // ots_evals
+        Schema::table('ots_evals', function (Blueprint $table) {
+            $table->foreign('form_id')
+                ->references('id')->on('ots_evals_forms')
+                ->onDelete('cascade');
+        });
+
+        //ots_evals_perf_cats
+        Schema::table('ots_evals_perf_cats', function (Blueprint $table) {
+            $table->foreign('form_id')
+                ->references('id')->on('ots_evals_forms')
+                ->onDelete('cascade');
+        });
+
+        //ots_evals_perf_indicators
+        Schema::table('ots_evals_perf_indicators', function (Blueprint $table) {
+            $table->foreign('perf_cat_id')
+                ->references('id')->on('ots_evals_perf_cats')
+                ->onDelete('cascade');
+        });
+
+        //ots_evals_indicator_results
+        Schema::table('ots_evals_indicator_results', function (Blueprint $table) {
+            $table->foreign('perf_indicator_id')
+                ->references('id')->on('ots_evals_perf_cats')
+                ->onDelete('cascade');
+            $table->foreign('eval_id')
+                ->references('id')->on('ots_evals')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
