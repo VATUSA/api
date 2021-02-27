ede51dcfba1a11c59ab6bcdf8d6664caea2123b7
Blake Nahin <bnahin@live.com>
2021-02-27 05:04:49 -0600

    Fix PublicController.php

--- app/Http/Controllers/API/v2/PublicController.php
+++ app/Http/Controllers/API/v2/PublicController.php
@@ -10,80 +10,74 @@ use Illuminate\Http\Request;
 class PublicController extends APIController
 {
 
-/**
- *
- * @SWG\Get(
- *     path="/public/events/(limit)",
- *     summary="Get events.",
- *     description="Get events (from Forums) set with a specific limit",
- *     tags={"user","public"},
- *     produces={"application/json"},
- *     @SWG\Parameter(name="limit", in="path", type="integer", description="Limit"),
- *     @SWG\Response(
- *         response="200",
- *         description="OK",
- *         @SWG\Schema(
- *             ref="#/definitions/OK"
- *         ),
- *         examples={"application/json":{"id_event":760,"start_date":"2000-03-30","end_date":"2000-03-30","id_board":0,"id_topic":0,"title":"FNOklahoma City","id_member":1021}}
- *     )
- * )
- * @param \Illuminate\Http\Request $request
- * @param                          $limit
- *
- * @return \Illuminate\Http\Response
- * @throws \Exception
- */
+    /**
+     *
+     * @SWG\Get(
+     *     path="/public/events/(limit)",
+     *     summary="Get events.",
+     *     description="Get events (from Forums) set with a specific limit",
+     *     tags={"user","public"},
+     *     produces={"application/json"},
+     *     @SWG\Parameter(name="limit", in="path", type="integer", description="Limit"),
+     *     @SWG\Response(
+     *         response="200",
+     *         description="OK",
+     *         @SWG\Schema(
+     *             ref="#/definitions/OK"
+     *         ),
+     *         examples={"application/json":{"id_event":760,"start_date":"2000-03-30","end_date":"2000-03-30","id_board":0,"id_topic":0,"title":"FNOklahoma
+     *         City","id_member":1021}}
+     *     )
+     * )
+     * @param \Illuminate\Http\Request $request
+     * @param                          $limit
+     *
+     * @return \Illuminate\Http\Response
+     * @throws \Exception
+     */
 
-public function getEvents(Request $request, $limit = 100)
-{
-    $data = ForumCalendar::limit($limit)
-        ->orderBy('start_date', 'asc')
-        ->get()
-        ->toArray();
+    public function getEvents(Request $request, $limit = 100)
+    {
+        $data = ForumCalendar::limit($limit)
+            ->orderBy('start_date', 'asc')
+            ->get()
+            ->toArray();
 
-    return response()->api([$data]);
-}
+        return response()->api([$data]);
+    }
 
-/**
- *
- * @SWG\Get(
- *     path="/public/news/(limits)",
- *     summary="Get news.",
- *     description="Get news (from Forums) set with a specific limit",
- *     tags={"user","public"},
- *     produces={"application/json"},
- *     @SWG\Parameter(name="limit", in="path", type="integer", description="Limit"),
- *     @SWG\Response(
- *         response="200",
- *         description="OK",
- *         @SWG\Schema(
- *             ref="#/definitions/OK"
- *         ),
- *         examples={"application/json":{{}
- *         "status"="OK",
- *         "testing"=false
- *         }
- *         },
- *     )
- * )
- * @param \Illuminate\Http\Request $request
- * @param                          $limit
- *
- * @return \Illuminate\Http\Response
- * @throws \Exception
- */
-public function getNews(Request $request, $limit = 100)
-{
-    $data = ForumMessages::where('id_board', 47)
-        ->orderByDesc('id_msg')
-        ->limit($limit)
-        ->get()
-        ->toArray();
+    /**
+     *
+     * @SWG\Get(
+     *     path="/public/news/(limits)",
+     *     summary="Get news.",
+     *     description="Get news (from Forums) set with a specific limit",
+     *     tags={"user","public"},
+     *     produces={"application/json"},
+     *     @SWG\Parameter(name="limit", in="path", type="integer", description="Limit"),
+     *     @SWG\Response(
+     *         response="200",
+     *         description="OK",
+     *         @SWG\Schema(
+     *             ref="#/definitions/OK"
+     *         )
+     *     )
+     * )
+     * @param \Illuminate\Http\Request $request
+     * @param                          $limit
+     *
+     * @return \Illuminate\Http\Response
+     * @throws \Exception
+     */
+    public function getNews(Request $request, $limit = 100)
+    {
+        $data = ForumMessages::where('id_board', 47)
+            ->orderByDesc('id_msg')
+            ->limit($limit)
+            ->get()
+            ->toArray();
 
-    return response()->api([$data]);
-}
+        return response()->api([$data]);
+    }
 
 }
-
-?>
