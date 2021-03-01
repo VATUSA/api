ce1a3e9453a5cf4de146d4834aa8e85a4f55b7b2
Blake Nahin <vatusa12@vatusa.net>
2021-02-28 21:43:55 -0600

    Preferred Name

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('stats:monthly')->monthlyOn(1, '00:00');
+        $schedule->command('moodle:sync')->twiceDaily();
     }
 
     /**
