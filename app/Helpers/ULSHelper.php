15e9bb1128966a79876cc3c4622753045b82449e
Blake Nahin <bnahin@live.com>
2021-06-19 01:35:22 -0500

    Forums redirect by environment

--- app/Helpers/ULSHelper.php
+++ app/Helpers/ULSHelper.php
@@ -70,8 +70,7 @@ class ULSHelper
         //smfapi_login($cid, 14400);
         \Auth::loginUsingId($cid, true);
 
-        if (!in_array(app()->environment(), ["livedev", "dev", "staging"])) {
-
+        if (in_array(app()->environment(), ["prod", "staging"])) {
             //Sync Moodle Roles
             $moodle = new VATUSAMoodle(false);
             if ($id = $moodle->getUserId($cid)) {
@@ -87,18 +86,9 @@ class ULSHelper
             $response = $moodle->request("auth_userkey_request_login_url",
                 ['user' => ['idnumber' => Auth::user()->cid]]);
             $url = $response["loginurl"];
-
-            $token = [
-                "cid"    => (string)$cid,
-                "nlt"    => time() + 7,
-                "return" => $return
-            ];
-            $token = static::base64url_encode(json_encode($token));
-            $signature = static::base64url_encode(hash_hmac("sha512", $token, base64_decode(env("FORUM_SECRET"))));
-
-            return redirect($url . "&wantsurl=" . urlencode("https://forums.vatusa.net/api.php?login=1&token=$token&signature=$signature"));
         }
-        if (app()->environment("staging")) {
+
+        if (!in_array(app()->environment(), ["dev", "local"])) {
             $token = [
                 "cid"    => (string)$cid,
                 "nlt"    => time() + 7,
@@ -107,8 +97,13 @@ class ULSHelper
             $token = static::base64url_encode(json_encode($token));
             $signature = static::base64url_encode(hash_hmac("sha512", $token, base64_decode(env("FORUM_SECRET"))));
 
-            return redirect("https://forums.staging.vatusa.net/api.php?login=1&token=$token&signature=$signature");
+            if (str_contains(config('app.url'), "staging")) {
+                $forumsUrl = str_replace("staging", "forums.staging", config('app.url'));
+            } else {
+                $forumsUrl = str_replace("www", "forums", config('app.url'));
+            }
 
+            return redirect($url . "&wantsurl=" . urlencode("$forumsUrl/api.php?login=1&token=$token&signature=$signature"));
         }
 
         return redirect(env('SSO_RETURN_HOME'));
