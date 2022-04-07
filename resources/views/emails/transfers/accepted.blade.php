0b3d4a0cbaeca904d7c1ab4b8c12cd4d630c8e4a
Alexandra Robison <46828856+arobison203@users.noreply.github.com>
2022-04-06 17:48:41 -0700

    fix typo in email

--- resources/views/emails/transfers/accepted.blade.php
+++ resources/views/emails/transfers/accepted.blade.php
@@ -1,6 +1,6 @@
 @extends('emails.layout')
 @section('title','Transfer Accepted')
 @section('content')
-    <p>A transfer request for {{$fname}} {{$lname}} ({{$cid}}) from {{ $from }} to {{ $to }} as been
+    <p>A transfer request for {{$fname}} {{$lname}} ({{$cid}}) from {{ $from }} to {{ $to }} has been
         <strong>accepted</strong>.<br>
 @endsection
