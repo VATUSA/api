6221fed5c4693d123081c2f48770c3f5dc2e9e94
Jon <jonhasacat8@gmail.com>
2022-05-03 20:52:39 -0700

    Allow senior staff to access training records via the API, even if not associated with their facility.
    This change is required because the VATUSA website reads the contents of training records from the API.

--- app/Http/Controllers/API/v2/TrainingController.php
+++ app/Http/Controllers/API/v2/TrainingController.php
@@ -1144,7 +1144,7 @@ class TrainingController extends Controller
                     Auth::user()->facility)->exists());
         $isTrainingStaff = Auth::user() && RoleHelper::isTrainingStaff(Auth::user()->cid, true,
                 $facility ?? Auth::user()->facility) && (!$record || ($record && ($record->student->facility == Auth::user()->facility || $record->facility->id == Auth::user()->facility)));
-        $isVATUSAStaff = Auth::user() && RoleHelper::isVATUSAStaff();
+        $isVATUSAStaff = Auth::user() && RoleHelper::isSeniorStaff(null, null, true);
         $ownsRecord = $record && Auth::user() && $record->student_id === Auth::user()->cid;
         $isOwnUser = Auth::user() && $user && $user->cid === Auth::user()->cid;
 
