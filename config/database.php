45198333e332f5eba5073c0c2dfaac6542452ebc
Jon <jonhasacat8@gmail.com>
2023-03-27 21:30:46 -0700

    Make redis use tls.

--- config/database.php
+++ config/database.php
@@ -109,6 +109,7 @@ return [
         'client' => 'predis',
 
         'default' => [
+            'scheme' => 'tls',
             'host'     => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port'     => env('REDIS_PORT', 6379),
