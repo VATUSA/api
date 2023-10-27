a96b2524cf4560efccbe93e43246ace0736c9834
Jon <jonhasacat8@gmail.com>
2023-10-26 21:12:37 -0700

    Don't mind me, just testing in prod

--- app/Http/Controllers/API/v2/IntegrationController.php
+++ app/Http/Controllers/API/v2/IntegrationController.php
@@ -10,6 +10,25 @@ use http\Env\Request;
 class IntegrationController extends APIController
 {
     public function getStaffMembers() {
+        function makeOutput($user) {
+            $c = $user->toArray();
+
+            unset($c['flag_broadcastOptedIn']);
+            unset($c['email']);
+            unset($c['flag_preventStaffAssign']);
+            unset($c['created_at']);
+            unset($c['updated_at']);
+            unset($c['flag_needbasic']);
+            unset($c['flag_xferOverride']);
+            unset($c['promotion_eligible']);
+            unset($c['transfer_eligible']);
+            unset($c['lastactivity']);
+            unset($c['last_cert_sync']);
+
+            $c['rating_short'] = RatingHelper::intToShort($c['rating']);
+            $c['visiting_facilities'] = $user->visits->toArray();
+            return $c;
+        }
         $staffRoles = ['ATM', 'DATM', 'TA', 'EC', 'FE', 'WM', 'INS', 'MTR'];
 
         $userRoles = Role::whereIn('role', $staffRoles)->get();
@@ -17,31 +36,13 @@ class IntegrationController extends APIController
         $controllers = [];
         foreach ($userRoles as $r) {
             $user = $r->user;
-            $c = $user->toArray();
-
-            unset($c['flag_broadcastOptedIn']);
-            unset($c['email']);
-            unset($c['flag_preventStaffAssign']);
-
-            $c['rating_short'] = RatingHelper::intToShort($c['rating']);
-            $c['visiting_facilities'] = $user->visits->toArray();
-
-            $controllers[$user->cid] = $c;
+            $controllers[$user->cid] = makeOutput($user);
         }
 
         $usersByRating = User::whereIn('rating', [8, 10])->get();
 
         foreach ($usersByRating as $user) {
-            $c = $user->toArray();
-
-            unset($c['flag_broadcastOptedIn']);
-            unset($c['email']);
-            unset($c['flag_preventStaffAssign']);
-
-            $c['rating_short'] = RatingHelper::intToShort($c['rating']);
-            $c['visiting_facilities'] = $user->visits->toArray();
-
-            $controllers[$user->cid] = $c;
+            $controllers[$user->cid] = makeOutput($user);
         }
 
 
