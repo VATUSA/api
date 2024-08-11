46fb594c2b18055c41e9d15f102b91b7f49cf368
Brandon Wening <brandonwening@gmail.com>
2024-08-11 18:46:38 -0400

    Changed moodle sync to every 3 hours

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('stats:monthly')->monthlyOn(1, '00:00');
-        $schedule->command('moodle:sync')->hourly();
+        $schedule->command('moodle:sync')->everyThreeHours($minutes = 0);
         $schedule->command('vatsim:flights')->everyMinute();
         $schedule->command('moodle:sendexamemails')->everyFiveMinutes();
     }
